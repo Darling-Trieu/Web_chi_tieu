e4976a7900f0784b9e096bc3223c735894d32593
Vani <vanixjnk@gmail.com>
2026-03-31 22:42:35 +0700

    feat: add application header and footer partials for consistent page layout

--- src/views/partials/application/AppHeader.php
+++ src/views/partials/application/AppHeader.php
@@ -4,6 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quản lý Chi Tiêu</title>
-    <link rel="stylesheet" href="/public/css/globals.css">
+    <link rel="stylesheet" href="/public/css/globals.css?<?php echo time(); ?>">
 </head>
 <body>
